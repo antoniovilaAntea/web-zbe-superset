9b0040444ef51b138c984e9ad0971642d75e3100
antoniovila <antoniovila@anteagroup.es>
2024-02-27 10:03:04 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <iframe
+          width="600"
+          height="400"
+          seamless
+          frameBorder="0"
+          scrolling="no"
+          src="http://localhost:8088/superset/explore/p/wY5pJdG4JxL/?standalone=1&height=400"
+        ></iframe>
+        <p>intneto</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
