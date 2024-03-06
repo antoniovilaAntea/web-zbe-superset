3ca619b4f55a6b5b741948d0fbd2cb3c28286097
antoniovila <antoniovila@anteagroup.es>
2024-03-06 11:02:17 +0100

    add: maps to monitorizacionRuido pages

---
+++ src/pages/monitorizacionRuido/monitorizacionRuido.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import MapRuido from "../../components/Map/MapRuido";
+
+import "./monitorizacionRuido.css";
+
+const MonitorizacionRuido = () => {
+  return (
+    <div className="monitorizacion">
+      <div className="monitorizacion__row">
+        {/* <div id="map"></div> */}
+        {/* <Mapmarkers></Mapmarkers>
+        <Mapmarkers></Mapmarkers> */}
+        <MapRuido id="map1"></MapRuido>
+        <MapRuido id="map2"></MapRuido>
+
+        {/* <div>Mapa b</div> */}
+        <div>
+          <iframe
+            width="400"
+            height="600"
+            seamless
+            frameBorder="0"
+            scrolling="no"
+            src="http://localhost:8088/superset/explore/p/rkVpXzr4yPJ/?standalone=1&height=600"
+          ></iframe>
+        </div>
+      </div>
+      <div className="monitorizacion__row">
+        <MapRuido id="map3"></MapRuido>
+        <MapRuido id="map4"></MapRuido>
+        <div>
+          <img src="https://i.ibb.co/ygCg6PC/ruido.png"></img>
+        </div>
+      </div>
+      <script src="./script.js"></script>
+    </div>
+  );
+};
+
+export default MonitorizacionRuido;
