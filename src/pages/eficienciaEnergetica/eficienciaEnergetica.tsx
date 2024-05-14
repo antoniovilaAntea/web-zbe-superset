9da976a08421c99d386a8a2365f0f4a0e51d4d77
antoniovila <antoniovila@anteagroup.es>
2024-05-14 13:31:45 +0200

    change: change graphics to eficiencia energetica

--- src/pages/eficienciaEnergetica/eficienciaEnergetica.tsx
+++ src/pages/eficienciaEnergetica/eficienciaEnergetica.tsx
@@ -1,14 +1,46 @@
 import React from "react";
 
+import "./eficienciaEnergetica.css";
+
 const EficienciaEnergetica = () => {
   return (
     <div className="eficiencia">
-      <iframe
-        height="500px"
-        width="1000px"
-        frameBorder={0}
-        src="http://localhost:8088/superset/dashboard/12/?standalone=3"
-      ></iframe>
+      <div className="emisiones">
+        <iframe
+          height="350px"
+          width="700px"
+          frameBorder={0}
+          src="http://localhost:8088/superset/explore/p/9RLpa3v47Bd/?standalone=3&height=350"
+        ></iframe>
+        <p>Emisiones (Tn CO²/año)</p>
+      </div>
+      <div className="consumo">
+        <iframe
+          height="350px"
+          width="700px"
+          frameBorder={0}
+          src="http://localhost:8088/superset/explore/p/jwW4V5ENag0/?standalone=3&height=350"
+        ></iframe>
+        <p>Consumo (TEP/año)</p>
+      </div>
+      <div className="combustible">
+        <iframe
+          height="350px"
+          width="700px"
+          frameBorder={0}
+          src="http://localhost:8088/superset/explore/p/2D64e184VKB/?standalone=3&height=350"
+        ></iframe>
+        <p>Combustible (litros)</p>
+      </div>
+      <div className="energia">
+        <iframe
+          height="350px"
+          width="700px"
+          frameBorder={0}
+          src="http://localhost:8088/superset/explore/p/DKep0Oglqgv/?standalone=3&height=350"
+        ></iframe>
+        <p>Energía consumida (kWh/año)</p>
+      </div>
     </div>
   );
 };
