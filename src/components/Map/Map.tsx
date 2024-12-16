d077a9a3a15a83323c62dc7e4d2050494ee21a20
tonovila17 <tonovila5@gmail.com>
2024-12-16 10:17:35 +0100

    fix: change zona3 color

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -180,7 +180,7 @@ const Mapa = (props: Props) => {
       </Polygon>
       <Polygon
         positions={zona3}
-        color={zona.includes("3") ? "#BDD72E" : "#61dafb"}
+        color={zona.includes("3") ? "#60B576" : "#61dafb"}
       >
         <Tooltip sticky>Zona 3</Tooltip>
       </Polygon>
