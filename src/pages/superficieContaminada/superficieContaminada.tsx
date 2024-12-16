352434dfeff5c9f83a685e2906d71eb2fe845fcb
tonovila17 <tonovila5@gmail.com>
2024-12-16 10:07:10 +0100

    feat: new table values

--- src/pages/superficieContaminada/superficieContaminada.tsx
+++ src/pages/superficieContaminada/superficieContaminada.tsx
@@ -5,16 +5,20 @@ import "./superficieContaminada.css";
 
 const SuperficieContaminada = () => {
   const [zona, setZona] = useState("");
-  const numpobZona1 = 8870;
-  const numpobZona2 = 23365;
-  const numsupZona1 = 2.2;
-  const numsupZona2 = 2.7;
+  const numpobZona1 = 48169;
+  const numpobZona2 = 28550;
+  const numpobZona3 = 60660;
+  const numsupZona1 = 2.35;
+  const numsupZona2 = 1;
+  const numsupZona3 = 1.9;
   const pobZona1 = "< " + numpobZona1.toString();
   const supZona1 = "< " + numsupZona1.toString();
   const pobZona2 = "< " + numpobZona2.toString();
   const supZona2 = "< " + numsupZona2.toString();
-  const pobTotal = "< " + (numpobZona1 + numpobZona2).toString();
-  const supTotal = "< " + (numsupZona1 + numsupZona2).toString();
+  const pobZona3 = "< " + numpobZona3.toString();
+  const supZona3 = "< " + numsupZona3.toString();
+  const pobTotal = "< " + (numpobZona1 + numpobZona2 + numpobZona3).toString();
+  const supTotal = "< " + (numsupZona1 + numsupZona2 + numsupZona3).toString();
   return (
     <div className="superficie">
       <Mapa zona={zona}></Mapa>
@@ -62,6 +66,23 @@ const SuperficieContaminada = () => {
             <td>
               <label id="superficieContaminadaC" />
               {pobZona2}
+            </td>
+          </tr>
+          <tr
+            onMouseEnter={() => setZona("Zona 3")}
+            onMouseLeave={() => setZona("")}
+          >
+            <td>
+              <label id="sectionC" />
+              Zona 3
+            </td>
+            <td>
+              <label id="periodInteresC" />
+              {supZona3}
+            </td>
+            <td>
+              <label id="superficieContaminadaC" />
+              {pobZona3}
             </td>
           </tr>
           <tr>
